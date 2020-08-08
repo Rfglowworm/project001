7eb0ab9f7343a90d26cac5e24a060f22c09d46df
xumm <xumm@163.com>
2020-08-08 20:43:39 +0800

    xumm commit

---
+++ src/test/java/com/luxb/xumm/project001/Project001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxb.xumm.project001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
